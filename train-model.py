4b8cbe1fd63b809c8c081ae233d689930c65dd80
Omar Makhlouf <omar.makhlouf93@gmail.com>
2018-04-23 16:30:50 +0100

    Minor changes to train-model.py
    Removed some magic numbers

--- train-model.py
+++ train-model.py
@@ -7,10 +7,11 @@ env = gym.make('MountainCarContinuous-v0')
 
 agg_iters = 7
 traj_per_agg = 9
+traj_length = 100
 controller_H = 8
 controller_K = 100
 # Make D_rand of random trajectories
-D_rand = get_random_data(env, num_rolls=10, max_roll_length=1000)
+D_rand = get_random_data(env, num_rolls=10, max_roll_length=traj_length)
 D_RL = Data(capacity = len(D_rand)*9)
 
 # Create model
@@ -22,7 +23,7 @@ for iteration in range(agg_iters):
 	agent.train(D_rand + D_RL, num_iters=50)
 	for i in range(traj_per_agg):
 		episode_reward = 0
-		for t in tqdm(range(1000)):
+		for t in tqdm(range(traj_length)):
 				action = agent.choose_action(state)
 				new_state, reward, done, info = env.step(action)
 				D_RL.pushTrajectory([state, action, new_state])
@@ -32,4 +33,4 @@ for iteration in range(agg_iters):
 				if done:
 					env.reset()
 		print('Trajectory ', i, 'reward: ', episode_reward)
-#	print(len(D_rand), len(D_RL))
+
