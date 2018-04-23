6ed05cfb67f61708a7c0ab5d4924f895622cf389
Henry Kenlay <kenlay@Henry.local>
2018-04-23 18:39:22 +0100

    Added more command line args

--- train-model.py
+++ train-model.py
@@ -1,70 +1,46 @@
-from Data import Data, get_random_data
-from Agent import Agent
+from Agent import MPCAgent
 from tqdm import tqdm
 import gym
 import argparse
 
 parser = argparse.ArgumentParser(description='Train a model using MPC')
-parser.add_argument('--environment', default='MountainCarContinuous-v0',
-                   help='Environment to train the agent on')
-parser.add_argument('--model-suffix', default='',
-                   help='Will save the model as [envname]-[suffix] if given')
-parser.add_argument("--no-render", default=False, action="store_true" , help="Flag to do something")
+parser.add_argument('--environment', default='MountainCarContinuous-v0', help='Environment to train the agent on')
+parser.add_argument('--experiment-name', default='', help='Will save the model as [envname]-[suffix] if given')
+parser.add_argument("--render", default=False, action="store_true",  help="Render environment")
+parser.add_argument("--softmax", default=False, action="store_true",  help="Use softmax MPC")
+parser.add_argument("--agg-iters", default=9, type=int, help="Aggregation iterations")
+parser.add_argument("--traj-per-agg", default=9, type=int, help="Number of rollouts per aggregation iterations")
+parser.add_argument("--traj-length", default=1000, type=int, help="Length of rollouts")
+parser.add_argument("--H", default=20, type=int, help="Horizon of the MPC controller")
+parser.add_argument("--K", default=250, type=int, help="Number of random rollouts of the MPC controller")
 args = parser.parse_args()
 
 env = gym.make(args.environment) 
 
-agg_iters = 7
-traj_per_agg = 9
-traj_length = 100
-controller_H = 8
-controller_K = 100
-# Make D_rand of random trajectories
-D_rand = get_random_data(env, num_rolls=10, max_roll_length=traj_length)
-D_RL = Data(capacity = len(D_rand)*9)
-
 # Create model
-<<<<<<< HEAD
-agent = Agent(env, controller_K=controller_K, controller_H=controller_H)
+agent = MPCAgent(env = env, H = args.H, K = args.K, traj_length = args.traj_length, softmax = args.softmax)
 
 # Main loops
-state = env.reset()
-for iteration in range(agg_iters):
-	agent.train(D_rand + D_RL, num_iters=50)
-	for i in range(traj_per_agg):
-		episode_reward = 0
-		for t in tqdm(range(traj_length)):
-				action = agent.choose_action(state)
-				new_state, reward, done, info = env.step(action)
-				D_RL.pushTrajectory([state, action, new_state])
-				state = new_state
-				episode_reward += reward
-				#env.render()
-				if done:
-					env.reset()
-		print('Trajectory ', i, 'reward: ', episode_reward)
 
-=======
-agent = Agent(env, softmax=True)
-
-# Main loops
-state = env.reset()
-for iteration in range(500):
-    agent.train(D_rand + D_RL)
-    for t in tqdm(range(250)):  
-        action = agent.choose_action(state)
-        new_state, reward, done, info = env.step(action)
-        D_RL.pushTrajectory([state, action, new_state])
-        state = new_state
-        if not args.no_render:
-            env.render()
-        if done:
-            env.reset()            
+for iteration in range(args.agg_iters):
+    agent.train(num_epochs = 60)
+    state = env.reset()
+    for _ in range(args.traj_per_agg):
+        total_reward = 0
+        for t in tqdm(range(args.traj_length)):  
+            action = agent.choose_action(state)
+            new_state, reward, done, info = env.step(action)
+            agent.D_RL.pushTrajectory([state, action, new_state])
+            state = new_state
+            if args.render:
+                env.render()
+            if done:
+                state = env.reset()  
+                print('Episode done. Total reward: {}'.format(total_reward))
+                break
+            total_reward += reward
+        print('Trajectory done. Total reward: {}'.format(total_reward))
     
-    print(len(D_rand), len(D_RL))
     
-if args.model_suffix == '':
-    model_name = args.environment
-else:
+if not args.experiment_name == '':
     model_name = '{}-{}'.format(args.environment, args.model_suffix)
->>>>>>> origin/master
