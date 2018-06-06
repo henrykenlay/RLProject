416136ad5e8e577888bf1c12539613cb95146d61
henry <henrykenlay@hotmail.com>
2018-06-06 12:26:13 +0100

    made logger more flexible

--- Logger.py
+++ Logger.py
@@ -2,16 +2,18 @@ import pandas as pd
 
 class Logger():
     
-    def __init__(self, log_dir, experiment_name):
+    def __init__(self, log_dir, experiment_name, columns = ['episodes', 'total_rewards', 'state_val', 'reward_val']):
         self.log_dir = log_dir
         self.experiment_name = experiment_name
         self.fname = '{}/{}.csv'.format(log_dir, experiment_name)
         self.data = []
+        self.columns = columns
     
-    def log(self, timestep, rewards, val_loss0, val_loss1):
-        self.data.append([timestep, rewards, val_loss0, val_loss1])
+    def log(self, values):
+        assert len(values) == len(self.columns)
+        self.data.append(values)
         self.makefile()
         
     def makefile(self):
-        data = pd.DataFrame(self.data, columns = ['episodes', 'total_rewards', 'state_val', 'reward_val'])
+        data = pd.DataFrame(self.data, columns = self.columns)
         data.to_csv(self.fname, index=False)
